a35b30d617c6240a37a2b2c337214e5f159ecdd6
Remy <shingiroremy303@gmail.com>
2026-01-26 20:10:28 +0200

    name

--- challenge_1.js
+++ challenge_1.js
@@ -1,14 +1,40 @@
+// async function getUserPosts(userId) {
+//   try{
+//     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+//     const posts = await response.json();
+    
+//     const result = {
+//       userId: userId,
+//       totalPosts: posts.length,
+//       posts: posts.map(post => post.title)
+//     };
+
+//     return result;
+    
+//   } catch(error) {
+//     console.error("Error:", error);
+//   }
+
+// }
+
+// // Test it:
+// getUserPosts(10  )
+//     .then(result => console.log(result));
+
+
 async function getUserPosts(userId) {
   try{
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    const name = await fetch("https://jsonplaceholder.typicode.com/users/");
+    const nom = await name.json();
     const posts = await response.json();
     
     const result = {
+      userName: nom.map(nam => nam.name),
       userId: userId,
       totalPosts: posts.length,
       posts: posts.map(post => post.title)
     };
-
     return result;
     
   } catch(error) {
@@ -18,5 +44,4 @@ async function getUserPosts(userId) {
 }
 
 // Test it:
-getUserPosts(10  )
-    .then(result => console.log(result));
+getUserPosts(10  ).then(result => console.log(result));
