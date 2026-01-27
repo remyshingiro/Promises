bbbbe7a7cedcbc71034c27facb5bef932120cb5a
Remy <shingiroremy303@gmail.com>
2026-01-27 16:33:22 +0200

    final challenge one

--- challenge_1.js
+++ challenge_1.js
@@ -1,36 +1,12 @@
-// async function getUserPosts(userId) {
-//   try{
-//     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-//     const posts = await response.json();
-    
-//     const result = {
-//       userId: userId,
-//       totalPosts: posts.length,
-//       posts: posts.map(post => post.title)
-//     };
-
-//     return result;
-    
-//   } catch(error) {
-//     console.error("Error:", error);
-//   }
-
-// }
-
-// // Test it:
-// getUserPosts(10  )
-//     .then(result => console.log(result));
-
-
 async function getUserPosts(userId) {
   try{
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-    const name = await fetch("https://jsonplaceholder.typicode.com/users/");
+    const name = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
     const nom = await name.json();
     const posts = await response.json();
     
     const result = {
-      userName: nom.map(nam => nam.name),
+      userName: nom.name,
       userId: userId,
       totalPosts: posts.length,
       posts: posts.map(post => post.title)
@@ -44,4 +20,5 @@ async function getUserPosts(userId) {
 }
 
 // Test it:
-getUserPosts(10  ).then(result => console.log(result));
+getUserPosts(4)
+  .then(result => console.log(result));
