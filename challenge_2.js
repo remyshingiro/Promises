2271a7f4c0e4ead5d20a81b9a0f54c9c90b0a2a3
Remy <shingiroremy303@gmail.com>
2026-01-29 07:28:09 +0200

    solved chall 2

--- challenge_2.js
+++ challenge_2.js
@@ -1,6 +1,94 @@
-async function getUserPosts(userId) {
+// async function getUserPosts(userId) {
+//     try {
+//         const users = await fetch("https://jsonplaceholder.typicode.com/users");
+//         const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+//         const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
 
+//         const allUsers =  users.json();
+//         const allPosts =  posts.json();
+//         const allComments = comments.json();
+
+//         userMap = {};
+//         allUsers.forEach(user => {
+//             userMap[user.id] = user.name;
+//         });
+
+//         commentsOfPosts = posts.map(post => {
+//             const commentCount = allComments.filter(comment => {
+//                 comment.postId === post.id
+//             }).length;
+//         });
+
+//         const authorName = userMap[post.userId];
+
+        
+
+//         return {
+//         postId: post.id,
+//         title: post.title,
+//         authorName: authorName,
+//         commentCount: commentCount
+//       }
+        
+//       commentsOfPosts.sort((a, b) => b.commentCount - a.commentCount);
+
+//       const topPosts = postsWithComments.slice(0, userId);
+
+//     } catch (error) {
+//         console.error("error:", error);
+        
+//     }
+
+// }
+
+// // Test it:
+// getUserPosts(1)
+//     .then(result => console.log(result));
+
+
+
+async function getTopPosts(num) {
+    try {
+        const [usersResponse, postsResponse, commentsResponse] = await Promise.all([
+            fetch("https://jsonplaceholder.typicode.com/users"),
+            fetch("https://jsonplaceholder.typicode.com/posts"),
+            fetch("https://jsonplaceholder.typicode.com/comments")
+        ]);
+
+        const [allUsers, allPosts, allComments] = await Promise.all([
+            usersResponse.json(),
+            postsResponse.json(),
+            commentsResponse.json()
+        ]);
+
+        const userMap = {};
+        allUsers.forEach(user => {
+            userMap[user.id] = user.name;
+        });
+
+        const postsWithComments = allPosts.map(post => {
+            const commentCount = allComments.filter(
+                comment => comment.postId === post.id  
+            ).length;
+            
+            const authorName = userMap[post.userId];
+
+            return {
+                postId: post.id,
+                title: post.title,
+                authorName: authorName,
+                commentCount: commentCount
+            };
+        });
+        postsWithComments.sort((a, b) => b.commentCount - a.commentCount);
+        const topPosts = postsWithComments.slice(0, num);
+        return topPosts;
+
+    } catch (error) {
+        console.error("Error:", error);
+        return null;
+    }
 }
 
 // Test it:
-getUserPosts(1).then(result => console.log(result));
+getTopPosts(5).then(result => console.log(result));
