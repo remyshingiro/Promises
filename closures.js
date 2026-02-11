ce5e71875284285dbda76df418f017fd01c66b35
Remy <shingiroremy303@gmail.com>
2026-02-11 20:06:50 +0200

    optimizing sort kata

--- closures.js
+++ closures.js
@@ -1,32 +1,80 @@
-function createFunction() {
-    function multiplyBy2 (num) {
-        return num * 2;
-    }
-    return multiplyBy2;
-}
+function outer () {
 
-const generatedFunc = createFunction();
-
-const result = generatedFunc(3);
-// console.log(result)
-
-
-
-
-
-
-
-function outer() {
     let counter = 0;
 
     function incrementCounter() {
         counter ++;
     }
-    incrementCounter();
-    // return counter;
+    
+    return incrementCounter;
 }
 
-console.log(outer())
+const myNewFunction = outer();
+console.log(myNewFunction())
+console.log(myNewFunction())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createFunction() {
+//     function multiplyBy2 (num) {
+//         return num * 2;
+//     }
+//     return multiplyBy2;
+// }
+
+// const generatedFunc = createFunction();
+
+// const result = generatedFunc(3);
+// // console.log(result)
+
+
+
+
+
+
+
+// function outer() {
+//     let counter = 0;
+
+//     function incrementCounter() {
+//         counter ++;
+//     }
+//     incrementCounter();
+//     // return counter;
+// }
+
+// console.log(outer())
 
 
 
@@ -35,18 +83,12 @@ console.log(outer())
 
 //calling a function outside of the function call in which it was defined
 // where U define your function determines what vars ur function has access to when you call the func.
-function outer () {
-    let counter = 0;
-    function incrementCounter() {
-        counter ++;
-        // return counter;
-    }
-    return incrementCounter;
-}
 
-const myNewFunction = outer();
-console.log(myNewFunction())
-console.log(myNewFunction())
+
+
+
+
+
 
 
 
